30ee959c5948845d39a77099f8fbcb223051743f
ShortTermAccount <alexander.fernandez.official@gmail.com>
2022-06-18 12:11:21 +0530

    first version of HelloWorld Added

--- src/com/java/helloWorld/HelloWorldProg.java
+++ src/com/java/helloWorld/HelloWorldProg.java
@@ -3,5 +3,6 @@ package com.java.helloWorld;
 public class HelloWorldProg {
     public static void main(String[] args){
         System.out.println("Hello World.");
+        System.out.println("First version added.");
     }
 }
